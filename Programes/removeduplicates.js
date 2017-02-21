027e2d88b6f91971f90a25f1a010a95f1bb5239c
unknown <inturi99@gmail.com>
2017-02-21 20:02:22 +0530

    include on other function in removedplicate

--- Programes/removeduplicates.js
+++ Programes/removeduplicates.js
@@ -1,7 +1,17 @@
-var arr = [1,1,2,2,3,4,2,1,6,6,7,8,8,9,9,9]
+var myarr = [1,1,2,2,3,4,2,1,6,6,7,8,8,9,9,9];
 
 var remomeduplicates = function (arr) {
     var temp =[]
-    for(i=0; i< arr.lengeth)
-	
+    for(var i=0; i < arr.length; i++){
+    	if(temp.indexOf(arr[i]) == -1) {
+    		temp.push(arr[i])
+    	}
+    }
+    return temp	
 }
+
+remomeduplicates(myarr)
+
+var result= myarr.filter(function(value,index,array) {
+	return  array.indexOf(value) == index;
+});
