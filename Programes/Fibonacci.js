e73e0845d5eede1e0f2aca6aa886334dea534c1a
Krishnarao inturi <inturi99@gmail.com>
2017-07-11 17:26:13 -0700

    add fibonacci

---
+++ Programes/Fibonacci.js
@@ -0,0 +1,19 @@
+var looping = function(n){
+	var a = 0, b = 1, f = 1;
+	for(var i = 2; i <= n; i++){
+		f = a + b;
+		a = b;
+		b = f;
+		console.log(f);
+	}
+	return f;
+}
+
+
+var recursive = function(n) {
+    if(n <= 2) {
+        return 1;
+    } else {
+        return this.recursive(n - 1) + this.recursive(n - 2);
+    }
+};
