e270ed69eadab46dde48057daa2ef074b081c85c
Krishnarao inturi <inturi99@gmail.com>
2017-07-19 10:58:25 -0700

    add reverse string

--- Programes/reverse.js
+++ Programes/reverse.js
@@ -1,9 +1,13 @@
-var reverse = function(s) {
+// reverse string
+var reversestring = function(s) {
 	var n = "";
 	for(var i = s.length - 1 ; i >= 0; i--){
      n += s[i];
 	}
   return n;
 }
+console.log(reversestring("KRISHNA"));
 
-console.log(reverse("KRISHNA"));
+//reverse number
+
+
