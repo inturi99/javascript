7349b796505c6cc80809c535bc01b3a46d3aa157
krishnarao inturi <inturi99@gmail.com>
2017-07-29 19:44:38 +0530

    Update factorial.js

--- Programes/factorial.js
+++ Programes/factorial.js
@@ -5,3 +5,17 @@ var factorial = function (n) {
 	}
 	return n * (n-1);
 }
+
+
+let range = function (start, stop){
+    var n = [];
+    for(var i = start; i < stop; i++){
+     n.push(i);
+    }
+    return n;
+}
+let factorial = n => {
+    return (range(1, n+1).reduce((x,y) => x * y, 1))
+}
+
+console.log(factorial(3))
